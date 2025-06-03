2f9275925b5094f836ab0cf59f34ad9bc6a8ed16
dadadadas111 <long6athcskl@gmail.com>
2025-06-03 17:33:16 +0700

    init localization

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,8 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function setLang(lang) {
+    // TODO: Implement real language switching (cookie, reload, etc.)
+    alert('Switch language to: ' + lang + ' (not implemented)');
+}
